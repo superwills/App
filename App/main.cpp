7b88b0495d39daed5b629d9a77f6c9d3b3ecb4a8
William Sherif <will.sherif@gmail.com>
2017-03-20 04:05:55 -0400

    Some comments

--- App/main.cpp
+++ App/main.cpp
@@ -5,11 +5,19 @@
 //  Created by William Sherif on 3/20/17.
 //  Copyright © 2017 William Sherif. All rights reserved.
 //
-
+// Regular comment
+/* Regular comment */
 #include <iostream>
 
+/// Doc-style comment
+/**
+  * Doc-style comment
+  * \param argc Number of arguments passed to the function
+  * \param argv List of command line arguments passed to the main function
+  * \returns integer indicating exit status of the program
+  */
 int main(int argc, const char * argv[]) {
   // insert code here...
   std::cout << "Hello, World!\n";
-    return 0;
+  return 0;
 }
