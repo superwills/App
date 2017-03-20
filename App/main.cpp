188b73f59b8e7a5d566750eaf6db1ecccfb77763
William Sherif <will.sherif@gmail.com>
2017-03-20 01:12:06 -0400

    Initial Commit

---
+++ App/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  App
+//
+//  Created by William Sherif on 3/20/17.
+//  Copyright © 2017 William Sherif. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
